272eb3102cda1f3178f12ab397e22bce9222a8dc
Raly04 <ralyandrynyainakadmiel@gmail.com>
2025-02-10 13:16:56 +0300

    Update project images and descriptions in Works component and translations

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -4,7 +4,9 @@ import Space from "./space";
 import Title from "./title";
 import WorkBox from "./work.box";
 import p1 from "../assets/images/Capture.png";
-import smmec from "../assets/images/image.png";
+import smmec from "../assets/images/smmec.png";
+import sal from "../assets/images/sal.png";
+import aceem from "../assets/images/aceem.png";
 import port from "../assets/images/portfolio.png";
 import { useTranslation } from "react-i18next";
 
@@ -50,10 +52,31 @@ export default function Works() {
     {
       image: smmec,
       technologies: t("works.3.technologies"),
-      title: t("works.2.title"),
+      title: t("works.3.title"),
       description: t("works.3.description"),
       isSecret: true,
     },
+    {
+      image: sal,
+      technologies: t("works.4.technologies"),
+      title: t("works.4.title"),
+      description: t("works.4.description"),
+      isSecret: true,
+    },
+    {
+      image: aceem,
+      technologies: t("works.5.technologies"),
+      title: t("works.5.title"),
+      description: t("works.5.description"),
+      isSecret: true,
+    },
+    // {
+    //   image: aceem,
+    //   technologies: t("works.6.technologies"),
+    //   title: t("works.6.title"),
+    //   description: t("works.6.description"),
+    //   isSecret: true,
+    // },
   ];
 
   return (
